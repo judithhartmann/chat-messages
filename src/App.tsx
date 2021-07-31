6c295e68670f7696b0f8c7835ed300b5804a8bcd
Judith Hartmann <mail@judith-hartmann.net>
2021-07-31 04:15:03 +0200

    setup message displays
    - add chat api calls
    - simple message rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import chatApi, { ChatMessage } from "./chatApi";
 
 function App() {
+  const [messages, setMessages] = useState<ChatMessage[]>([]);
+  useEffect(() => {
+    async function loadMessages() {
+      const loadedMessages = await chatApi.getMessages();
+      setMessages(loadedMessages);
+      console.log(loadedMessages);
+    }
+    loadMessages();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {messages.map((message) => (
+        <div key={message._id}>
+          <span>{message.author}</span>
+          <span>{message.message}</span>
+          <span>{message.timestamp}</span>
+        </div>
+      ))}
+    </>
   );
 }
 
---
+++ src/chatApi.ts
@@ -0,0 +1,27 @@
+export interface ChatMessage {
+  message: string;
+  author: string;
+  timestamp: string;
+  _id: string;
+}
+export class ChatApi {
+  apiToken: string;
+  baseUrl = "https://chatty.kubernetes.doodle-test.com/api/chatty/v1.0/";
+
+  constructor(apiToken: string) {
+    this.apiToken = apiToken;
+  }
+
+  async getMessages(): Promise<ChatMessage[]> {
+    const response = await fetch(`${this.baseUrl}?token=${this.apiToken}`);
+    return await response.json();
+  }
+}
+
+if (!process.env.REACT_APP_API_TOKEN) {
+  throw new Error("API TOKEN MISSING, Please add it .env file");
+}
+
+const chatApi = new ChatApi(process.env.REACT_APP_API_TOKEN);
+
+export default chatApi;
